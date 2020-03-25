9d69daa9d818d127d58a51009ccaa560933827b0
Michael Seymour <michael.seymour@booking.com>
2020-03-25 20:06:32 +0100

    Update various things

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ window.onload = initAll;
 var cells = [
     "Hi, who just joined?",
     "Can you email that to everyone?",
-    "_ you're on mute",
     "Uh, _, you're still sharing your screen...",
     "Sorry guys, I've got to jump to another call",
     "*sound of someone typing furiously*",
+    "_ you're on mute",
     "Sorry, I was on mute",
     "Is _ on the call?",
     "Can everyone go on mute?",
@@ -69,34 +69,46 @@ function toggleColor(evt) {
 		var thisSquare = window.event.srcElement;
 	}
     thisSquare.classList.toggle("active");
-	//checkWin();
+	checkWin();
 }
 
 function checkWin() {
-	var winningOption = -1;
-	var setSquares = 0;
-	var winners = new Array(31,992,15360,507904,541729,557328,1083458,2162820,4329736,8519745,8659472,16252928);
+    var setSquares = 0;
+    var winningSquares = 0;
+    var winners = [
+        2**0|2**5|2**10|2**14|2**19,//row 0
+        2**1|2**6|2**11|2**15|2**20,//row 1
+        2**2|2**7|2**24|2**16|2**21,//row 2
+        2**3|2**8|2**12|2**17|2**22,//row 3
+        2**4|2**9|2**13|2**18|2**23,//row 4
+        2**0|2**1|2**2|2**3|2**4,//col 0
+        2**5|2**6|2**7|2**8|2**9,//col 1
+        2**10|2**11|2**24|2**12|2**13,//col 2
+        2**14|2**15|2**16|2**17|2**18,//col 3
+        2**19|2**20|2**21|2**22|2**23,//col 4
+        2**0|2**6|2**24|2**17|2**23,//diag 0
+        2**19|2**15|2**24|2**8|2**4,//diag 1
+    ]
 
-	for (var i=0; i<24; i++) {
+	for (var i=0; i<25; i++) {
 		var currSquare = "square" + i;
-		if (document.getElementById(currSquare).className != "") {
-			document.getElementById(currSquare).className = "pickedBG";
-			setSquares = setSquares | Math.pow(2,i);
+		if (document.getElementById(currSquare).classList.contains("active")) {
+			setSquares = setSquares | 2 ** i;
 		}
 	}
 
 	for (var i=0; i<winners.length; i++) {
 		if ((winners[i] & setSquares) == winners[i]) {
-			winningOption = i;
+            winningSquares = winningSquares | winners[i];
 		}
 	}
 	
-	if (winningOption > -1) {
-		for (var i=0; i<24; i++) {
-			if (winners[winningOption] & Math.pow(2,i)) {
-				currSquare = "square" + i;
-				document.getElementById(currSquare).className = "winningBG";
-			}
-		}
-	}
+    for (var i=0; i<25; i++) {
+        currSquare = "square" + i;
+        if (winningSquares & Math.pow(2,i)) {
+            document.getElementById(currSquare).classList.add("winningBG");
+        } else {
+            document.getElementById(currSquare).classList.remove("winningBG");
+        }
+    }
 };
